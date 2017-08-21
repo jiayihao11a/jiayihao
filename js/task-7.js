6efb5348d9de17c093e1ffa015f16f13fe67a96c
jiayihao11a <31180782+jiayihao11a@users.noreply.github.com>
2017-08-21 15:35:57 +0800

    task
    我自己练习的web前端小项目

---
+++ js/task-7.js
@@ -0,0 +1,8 @@
+function contxt() //¶¨Òåº¯Êý
+      {
+		 if($("#test").hasClass("row-10-on")){
+                        $("#test").removeClass("row-10-on").addClass("row-10-dowm");
+                    }else{
+                        $("#test").removeClass("row-10-dowm").addClass("row-10-on");
+                    }
+      }
